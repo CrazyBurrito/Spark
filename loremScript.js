f954546da0debb244256e7f7f8f507d2a05b64f0
CzarcastiacJack <joshua.weaver@colorado.edu>
2016-04-06 17:28:03 -0600

    Finished Lorem Ipsum

--- loremScript.js
+++ loremScript.js
@@ -1,4 +1,58 @@
+var array = ["card","deck","set","play","game","people","time","creature","players","mana","lands","magic","pretty","opponent","mean","playing","jace","standard","probably","modern","draw","spell","format","win","bad","eldrazi","ability","cost","art","feel","hand","color","innistrad","makes","control","work","yeah","story","cast","top","love","guy","madness","fun","damage","look","block","thought","rules","event"];
+var paragraphText = "";
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+$("#ipsum").hide();
+
 $("#generate").click(function(){
+    paragraphText = "";
     var numParagraphs = $("#numInput").val();
-    $("p").html(numParagraphs);
+    
+    if(numParagraphs > 99){
+        $("#numInput").val(99);
+        numParagraphs = 99;
+    }
+    else if(numParagraphs < 1){
+        $("#numInput").val(1);
+        numParagraphs = 1;
+    }
+    var paragraphLength = 0;
+    var sentanceLength = 0;
+    var index = 0;
+    var cap = true;
+    
+    for(j=0; j<numParagraphs; j++){
+        paragraphLength = (Math.floor((Math.random()*100)+90));
+        sentanceLength = (Math.floor(Math.random()*10)+15);
+        
+        for(i=0; i<paragraphLength; i++){
+            index = Math.floor(Math.random()*array.length);
+            console.log(sentanceLength);
+            
+            if(cap){
+                paragraphText+= capitalizeFirstLetter(array[index]);
+                cap = false;
+            }
+            else
+                paragraphText+= array[index];
+                
+            if(i == sentanceLength && i != paragraphLength - 1){
+                paragraphText+= "."
+                sentanceLength = i + (Math.floor(Math.random()*10)+15)
+                cap = true;
+                }
+            if(i != paragraphLength - 1)  
+                paragraphText+= " ";
+        }
+        paragraphText+=".<br><br>";
+        cap = true;
+    }
+        
+    $("#ipsum").html(paragraphText);
+    
+    $("#ipsum").show();
+    
 });

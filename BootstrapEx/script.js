3f64ffdaedd575569fd2259ae8d145f4f18a26b2
CzarcastiacJack <joshua.weaver@colorado.edu>
2016-04-18 18:38:52 -0600

    Reorganized file structure

---
+++ BootstrapEx/script.js
@@ -0,0 +1,26 @@
+var x =1;
+console.log(x);
+var x = "a string";
+console.log(x);
+var x = null;
+console.log(x);
+var myArray = [1, "two", "3"];
+console.log(myArray);
+console.log(myArray[0]);
+myArray[1] = "element 1";
+console.log(myArray);
+console.log(myArray.length);
+var user = {
+    
+        whoa:{dude:'sweet'}
+};
+console.log(user);
+for(var i=1; i<=100; i++){
+    
+    console.log(i);
+}
+function addTwoNumbers(a, b){
+    
+    return a + b;
+};
+console.log(addTwoNumbers(1,2));
